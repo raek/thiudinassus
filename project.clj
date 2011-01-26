61c47cadcddfdd8ca0fbdf0e127752518d8eb735
Rasmus Svensson <raek@lysator.liu.se>
2011-01-27 00:40:22 +0100

    New version of Marginalia

--- project.clj
+++ project.clj
@@ -2,4 +2,4 @@
   :description "Sa Þiudinassus (𐍃𐌰 𐌸𐌹𐌿𐌳𐌹𐌽𐌰𐍃𐍃𐌿𐍃) is a computer board game heavily insipired by Carcassonne."
   :dependencies [[org.clojure/clojure "1.2.0"]]
   :dev-dependencies [[swank-clojure "1.2.1"]
-                     [marginalia "0.3.2"]])
+                     [marginalia "0.5.0-alpha"]])
